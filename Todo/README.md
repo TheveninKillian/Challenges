c7c65fbdced4b2d76f53484653b94c6efee2a80d
Killian <thevenin.killian@gmail.com>
2021-01-14 17:21:13 +0100

    create app todo list for challenges

---
+++ Todo/README.md
@@ -0,0 +1,47 @@
+<!-- Please update value in the {}  -->
+
+<h1 align="center">Todo app</h1>
+
+<div align="center">
+   Solution for a challenge from  <a href="http://devchallenges.io" target="_blank">Devchallenges.io</a>.
+</div>
+
+<div align="center">
+  <h3>
+    <a href="https://{your-demo-link.your-domain}">
+      Demo
+    </a>
+    <span> | </span>
+    <a href="https://{your-url-to-the-solution}">
+      Solution
+    </a>
+    <span> | </span>
+    <a href="https://devchallenges.io/challenges/hH6PbOHBdPm6otzw2De5">
+      Challenge
+    </a>
+  </h3>
+</div>
+
+<!-- OVERVIEW -->
+
+## Overview
+
+<div align='center'>
+  <img src='./screenshot.jpg' width='675'>
+</div>
+
+### Built With
+
+<!-- This section should list any major frameworks that you built your project using. Here are a few examples.-->
+
+- [Vue.js](https://vuejs.org/)
+
+## Features
+
+This application/site was created as a submission to a [DevChallenges](https://devchallenges.io/challenges) challenge. The [challenge](https://devchallenges.io/challenges/hH6PbOHBdPm6otzw2De5) was to build an application to complete the given user stories.
+
+## Contact
+
+- Website [Killian-thevenin.netlify.app](https://killian-thevenin.netlify.app/)
+- GitHub [@TheveninKillian](https://github.com/TheveninKillian)
+- Twitter [@Killian_Thvn](https://twitter.com/Killian_Thvn)
---
+++ Todo/app.js
@@ -0,0 +1,158 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      currentTab: 'All',
+      inputValue: '',
+      tabs: [
+        { name: 'All', class: 'tab-all' },
+        { name: 'Active', class: 'tab-active' },
+        { name: 'Completed', class: 'tab-completed' },
+      ],
+      items: [
+        {
+          todo: 'Do coding challenges 1',
+          completed: 'no-completed',
+          id: 'tab-1',
+        },
+        {
+          todo: 'Do coding challenges 2',
+          completed: 'completed',
+          id: 'tab-2',
+        },
+        {
+          todo: 'Do coding challenges 3',
+          completed: 'no-completed',
+          id: 'tab-3',
+        },
+      ],
+      checkedItems: ['tab-2'],
+      initId: 3,
+      storageCheckItems: '',
+      storageItems: '',
+    };
+  },
+  mounted() {
+    this.storageCheckItems = localStorage.getItem('checkItems');
+    this.storageItems = localStorage.getItem('items');
+
+    if (this.storageCheckItems !== null) {
+      this.checkedItems = this.storageCheckItems.split(',');
+    }
+
+    if (this.storageItems !== null) {
+      this.items = JSON.parse(this.storageItems);
+    }
+  },
+  updated() {
+    if (this.items.length === 0) {
+      this.initId = 0;
+    }
+  },
+  methods: {
+    checkItem(item) {
+      const elementItem = document.getElementById(item.id);
+
+      if (elementItem.checked) {
+        this.checkedItems.push(item.id);
+
+        item.completed = 'completed';
+
+        this.tabNavigation();
+      } else {
+        this.checkedItems.forEach((element, key) => {
+          if (element === item.id) {
+            this.checkedItems.splice(key, 1);
+          }
+        });
+
+        item.completed = 'no-completed';
+
+        this.tabNavigation();
+      }
+
+      this.backupStorage();
+    },
+    tabNavigation() {
+      if (this.currentTab === 'All') {
+        this.items.forEach((element) => {
+          let item = document.getElementById(element.id);
+
+          item.parentElement.style.display = 'block';
+        });
+      }
+
+      if (this.currentTab === 'Active') {
+        this.items.forEach((element) => {
+          let item = document.getElementById(element.id);
+
+          if (element.completed === 'completed') {
+            item.parentElement.style.display = 'none';
+          } else {
+            item.parentElement.style.display = 'block';
+          }
+        });
+      }
+
+      if (this.currentTab === 'Completed') {
+        this.items.forEach((element) => {
+          let item = document.getElementById(element.id);
+
+          if (element.completed !== 'completed') {
+            item.parentElement.style.display = 'none';
+          } else {
+            item.parentElement.style.display = 'block';
+          }
+        });
+      }
+    },
+    delItem(item) {
+      this.items.forEach((element, key) => {
+        if (element.id === item.id) {
+          this.items.splice(key, 1);
+        }
+      });
+
+      this.checkedItems.forEach((element, key) => {
+        if (element === item.id) {
+          this.checkedItems.splice(key, 1);
+        }
+      });
+
+      this.backupStorage();
+    },
+    delAll() {
+      this.checkedItems.forEach((elementCheck) => {
+        this.items.forEach((element, key) => {
+          if (elementCheck === element.id) {
+            this.items.splice(key, 1);
+          }
+        });
+      });
+
+      this.checkedItems = [];
+
+      this.backupStorage();
+    },
+    addNewItem(text) {
+      let newItem = {};
+      newItem.todo = text;
+      newItem.completed = 'no-completed';
+      newItem.id = 'tab-' + this.initId++;
+
+      this.items.push(newItem);
+
+      this.inputValue = '';
+      let stringItems = JSON.stringify(this.items);
+      localStorage.setItem('items', stringItems);
+    },
+    backupStorage() {
+      let stringCheckItems = this.checkedItems.toString();
+      let stringItems = JSON.stringify(this.items);
+
+      localStorage.setItem('checkItems', stringCheckItems);
+      localStorage.setItem('items', stringItems);
+    },
+  },
+});
+
+const vm = app.mount('#app');
