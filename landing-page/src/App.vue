32517e43a33db43e2ce46b4314497c63bbe898ca
Killian <thevenin.killian@gmail.com>
2021-04-26 15:10:38 +0200

    init project

---
+++ landing-page/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div></div>
+</template>
+
+<script></script>
+
+<style lang="scss">
+#app {
+  position: relative;
+
+  margin-top: 60px;
+  padding-left: calc(50vw - 570px);
+  padding-right: calc(50vw - 570px);
+
+  font-family: "Rubik", sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
