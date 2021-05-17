74b2f10bbd7b36bda77b6ad3530a7d2166624b51
Killian <thevenin.killian@gmail.com>
2021-05-17 15:56:22 +0200

    delete Count and create Banner

---
+++ landing-page/src/components/Banner.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="w-full text-center bg-black text-white font-bold p-4">
+    🎉 To celebrate Spence's launch, we will be waiving all fees for the entire
+    month of April.
+    <span class="underline">You will be receiving 100% of the earnings.</span>🎉
+  </div>
+</template>
