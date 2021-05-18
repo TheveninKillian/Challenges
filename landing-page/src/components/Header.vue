701419380895f63d66e80fdf6961d076f28ad2ad
Killian <thevenin.killian@gmail.com>
2021-05-18 12:27:07 +0200

    create new components

---
+++ landing-page/src/components/Header.vue
@@ -0,0 +1,53 @@
+<template>
+  <header class="flex justify-between items-center py-8">
+    <h1 class="cursor-pointer">spence</h1>
+
+    <div class="header__hbg">
+      <img
+        src="../assets/images/Hamburger Menu.svg"
+        alt="menu de navigation pour mobile"
+      />
+    </div>
+
+    <nav class="header__nav">
+      <ul class="flex">
+        <li>Business</li>
+        <li>Pricing</li>
+        <li>Features</li>
+      </ul>
+    </nav>
+
+    <nav class="header__nav">
+      <ul class="flex">
+        <li>Login</li>
+        <li>Get Started</li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style lang="scss">
+@import "../assets/sass/mixins";
+
+.header {
+  &__hbg {
+    cursor: pointer;
+
+    @include breakpoint(sm) {
+      display: none;
+    }
+  }
+
+  &__nav {
+    display: none;
+
+    @include breakpoint(sm) {
+      display: block;
+    }
+
+    li {
+      cursor: pointer;
+    }
+  }
+}
+</style>
---
+++ landing-page/src/components/Hero.vue
@@ -0,0 +1,62 @@
+<template>
+  <section class="hero">
+    <div class="hero__img">
+      <img src="../assets/images/Hero Image.png" />
+    </div>
+
+    <div class="hero__wrapper">
+      <h2 class="text-3xl font-bold">
+        Share your unfiltered thoughts. Get paid.
+      </h2>
+
+      <p>
+        Spense is an open platform for individuals to share their unfiltered
+        thoughts. Discuss the topics you love, and get paid for doing
+        <span class="italic">just</span> that.
+      </p>
+
+      <ul>
+        <li>Receive 99% off the earnings.</li>
+        <li>Get paid via Debit Card, ACH, or Paypal.</li>
+        <li>Withdraw your earnings anytime</li>
+      </ul>
+
+      <div>
+        <input type="text" />
+        <input type="submit" value="Get Started" />
+      </div>
+    </div>
+  </section>
+</template>
+
+<style lang="scss">
+@import "../assets/sass/mixins";
+
+.hero {
+  @include breakpoint(md) {
+    display: flex;
+    align-items: center;
+
+    &__img {
+      order: 1;
+
+      width: 45%;
+    }
+
+    &__wrapper {
+      width: 55%;
+    }
+  }
+
+  li {
+    &::before {
+      content: "";
+      float: left;
+      width: 24px;
+      height: 24px;
+      margin-right: 15px;
+      background-image: url("../assets/images/Checkmark.svg");
+    }
+  }
+}
+</style>
