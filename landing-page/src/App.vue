8600cfd4f56c9f3aec44930f2f30bc4d3879a8f1
Killian <thevenin.killian@gmail.com>
2021-05-18 12:27:38 +0200

    add components Header and Hero + style

--- landing-page/src/App.vue
+++ landing-page/src/App.vue
@@ -2,4 +2,51 @@
 
 <template>
   <Banner />
+
+  <div class="main">
+    <Header />
+    <Hero />
+  </div>
 </template>
+
+<style lang="scss">
+@import "./assets/sass/mixins";
+
+.main {
+  padding-left: calc(50vw - 150px);
+  padding-right: calc(50vw - 150px);
+  padding-bottom: 3rem;
+
+  background-color: #f0eeeb;
+
+  @include breakpoint(xs) {
+    padding-left: calc(50vw - 250px);
+    padding-right: calc(50vw - 250px);
+  }
+
+  @include breakpoint(sm) {
+    padding-left: calc(50vw - 325px);
+    padding-right: calc(50vw - 325px);
+  }
+
+  @include breakpoint(md) {
+    padding-left: calc(50vw - 400px);
+    padding-right: calc(50vw - 400px);
+  }
+
+  @include breakpoint(lg) {
+    padding-left: calc(50vw - 500px);
+    padding-right: calc(50vw - 500px);
+  }
+
+  @include breakpoint(xl) {
+    padding-left: calc(50vw - 550px);
+    padding-right: calc(50vw - 550px);
+  }
+
+  @include breakpoint(xxl) {
+    padding-left: calc(50vw - 670px);
+    padding-right: calc(50vw - 670px);
+  }
+}
+</style>
