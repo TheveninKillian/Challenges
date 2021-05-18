0ddabe44b78567c97d75f66c1942655a10293436
Killian <thevenin.killian@gmail.com>
2021-05-18 12:26:24 +0200

    add padding

--- landing-page/src/components/Banner.vue
+++ landing-page/src/components/Banner.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full text-center bg-black text-white font-bold p-4">
+  <div class="w-full text-center bg-black text-white font-bold py-4 px-2">
     🎉 To celebrate Spence's launch, we will be waiving all fees for the entire
     month of April.
     <span class="underline">You will be receiving 100% of the earnings.</span>🎉
