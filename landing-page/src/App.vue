894d8e4c1e518dc001e16f0593b6563570f310d0
Killian <thevenin.killian@gmail.com>
2021-05-18 15:53:41 +0200

    edit style

--- landing-page/src/App.vue
+++ landing-page/src/App.vue
@@ -13,35 +13,35 @@
 @import "./assets/sass/mixins";
 
 .main {
-  padding-left: calc(50vw - 150px);
-  padding-right: calc(50vw - 150px);
+  padding-left: calc(50vw - 170px);
+  padding-right: calc(50vw - 170px);
   padding-bottom: 3rem;
 
   background-color: #f0eeeb;
 
   @include breakpoint(xs) {
-    padding-left: calc(50vw - 250px);
-    padding-right: calc(50vw - 250px);
+    padding-left: calc(50vw - 270px);
+    padding-right: calc(50vw - 270px);
   }
 
   @include breakpoint(sm) {
-    padding-left: calc(50vw - 325px);
-    padding-right: calc(50vw - 325px);
+    padding-left: calc(50vw - 350px);
+    padding-right: calc(50vw - 350px);
   }
 
   @include breakpoint(md) {
-    padding-left: calc(50vw - 400px);
-    padding-right: calc(50vw - 400px);
+    padding-left: calc(50vw - 450px);
+    padding-right: calc(50vw - 450px);
   }
 
   @include breakpoint(lg) {
-    padding-left: calc(50vw - 500px);
-    padding-right: calc(50vw - 500px);
+    padding-left: calc(50vw - 550px);
+    padding-right: calc(50vw - 550px);
   }
 
   @include breakpoint(xl) {
-    padding-left: calc(50vw - 550px);
-    padding-right: calc(50vw - 550px);
+    padding-left: calc(50vw - 600px);
+    padding-right: calc(50vw - 600px);
   }
 
   @include breakpoint(xxl) {
