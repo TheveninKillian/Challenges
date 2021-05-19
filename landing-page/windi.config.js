c85dc21088ee39531bf059e1dc5924df40d2ed2a
Killian <thevenin.killian@gmail.com>
2021-05-19 15:44:29 +0200

    uptade config responsive

--- landing-page/windi.config.js
+++ landing-page/windi.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
         sm: '640px',
         md: '768px',
         lg: '1024px',
-        xl: '1280px',
+        xl: '1440px',
         '2xl': '1536px',
       },
       fontFamily: {
